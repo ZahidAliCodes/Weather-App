515b965de091b644a1a98b706ae7a42f154642c5
Zahid Ali <155419815+ZahidAliCodes@users.noreply.github.com>
2024-07-05 10:38:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -25,24 +25,24 @@ async function checkWeather(city) {
         document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
 
         if (data.weather[0].main == "Clouds") {
-            weatherIcon.src = "images/clouds.png";
-            weatherStatus.src = "images/clouds.png";
+            weatherIcon.src = "./clouds.png";
+            weatherStatus.src = "./clouds.png";
         }
         else if (data.weather[0].main == "Clear") {
-            weatherIcon.src = "images/clear.png";
-            weatherStatus.src = "images/clear.png";
+            weatherIcon.src = "./clear.png";
+            weatherStatus.src = "./clear.png";
         }
         else if (data.weather[0].main == "Rain") {
-            weatherIcon.src = "images/rain.png";
-            weatherStatus.src = "images/rain.png";
+            weatherIcon.src = "./rain.png";
+            weatherStatus.src = "./rain.png";
         }
         else if (data.weather[0].main == "Drizzle") {
-            weatherIcon.src = "images/drizzle.png";
-            weatherStatus.src = "images/drizzle.png";
+            weatherIcon.src = "./drizzle.png";
+            weatherStatus.src = "./drizzle.png";
         }
         else if (data.weather[0].main == "Mist") {
-            weatherIcon.src = "images/clear.png";
-            weatherStatus.src = "images/clear.png";
+            weatherIcon.src = "./clear.png";
+            weatherStatus.src = "./clear.png";
         }
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
